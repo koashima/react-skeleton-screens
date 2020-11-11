2f84fadac44d864d4db23c27e192c0d291e400fa
koashima <48111257+koashima@users.noreply.github.com>
2020-11-10 19:52:52 -0800

    fetch and display article dummy data with 5 sec timeout

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Articles = () => { 
-  return(
-    <div class="articles">
-      <h2>ARTICLES</h2>
-    </div>
-  )
-}
+const Articles = () => {
+  const [articles, setArticles] = useState(null);
+
+  useEffect(() => {
+    setTimeout(async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const data = await res.json();
+      setArticles(data);
+    }, 5000);
+  });
+  return (
+  <div class="articles">
+    <h2>ARTICLES</h2>
+
+    {articles && articles.map(article => (
+      <div class="article" key={article.id}>
+        <h3>{article.title}</h3>
+        <p>{article.body}</p>
+      </div>
+    ))}
+
+    {!articles && (
+      <div>
+        <h1>LOADING...</h1>
+      </div>
+    )}
+    
+  </div>
+);
+};
 
 export default Articles;
